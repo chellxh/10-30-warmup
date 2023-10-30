928e5d83412e7ad04a82cbf1837995c105ab9640
Michelle Harley <chell.hx@gmail.com>
2023-10-30 18:21:25 -0400

    added my name

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function time() {
   const today = new Date();
   let hour = today.getHours();
   let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
+    let seconds = today.getSeconds();
+    
   minutes = checkTime(minutes);
   seconds = checkTime(seconds);
   hour = hour > 12 ? hour - 12 : hour;
-  hour = hour == 0 ? 12 : hour;
+    hour = hour == 0 ? 12 : hour;
+    
   document.getElementById("time").innerHTML =
     hour + ":" + minutes + ":" + seconds;
   setTimeout(time, 1000);
